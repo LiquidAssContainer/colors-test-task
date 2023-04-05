7ba14eb9e7374cf3a6113f1576cdb2f2978b1b50
LiquidAssContainer <47602463+LiquidAssContainer@users.noreply.github.com>
2023-04-05 15:57:32 +0600

    feat: add checkboxes, unfinished cart & product items

---
+++ src/shared/ui/components/SliderCheckbox/SliderCheckbox.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const {
+  name,
+  label,
+  hasText = true,
+} = defineProps<{ name: string; label: string; hasText?: boolean }>();
+</script>
+
+<template lang="pug">
+label
+    input(type="checkbox" :name="name")
+    .visual
+    .text {{label}}
+</template>
+
+<style lang="sass" scoped>
+label
+    display: flex
+    align-items: center
+    // gap: 12px
+
+.visual
+    position: relative
+    display: block
+    height: 22px
+    width: 36px
+    margin-right: 12px
+
+    background: #F2F2F2
+    border-radius: 40px
+
+    cursor: pointer
+    transition: .2s all ease-in-out
+
+input:checked ~ .visual
+    background: #7BB899
+
+    &::before
+        left: 21px
+
+
+input
+    appearance: none
+
+.visual::before
+    content: ''
+    position: absolute
+    width: 8px
+    height: 8px
+    left: 7px
+    top: 7px
+    border-radius: 50%
+
+    background: #1F2020
+
+.text
+    font-size: 12px
+    line-height: 100%
+
+    letter-spacing: 0.06em
+    text-transform: uppercase
+
+
+    color: #1F2020
+</style>
---
+++ src/widgets/cart/CartItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { img, price, amount, name } = defineProps<{
+  img;
+  price;
+  amount;
+  name;
+}>();
+</script>
+
+<template lang="pug">
+li.cart-item
+    img
+    .cart-item__info
+</template>
+
+<style lang="sass" scoped></style>
