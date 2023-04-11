06523d32c6d4dc1fea34bae4a1d25dbee66b403f
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2023-04-11 10:21:16 +0600

    chore: fix some configs for TypeScript checks

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,17 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgLoader from 'vite-svg-loader';
+// import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   base: '',
-  plugins: [vue(), vueJsx(), tsconfigPaths(), svgLoader()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    tsconfigPaths(),
+    svgLoader(),
+    // checker({ vueTsc: true }),
+  ],
   css: {
     preprocessorOptions: {
       sass: {
